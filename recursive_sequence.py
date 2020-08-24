7f09d3b3b6bbb13f56024032aa883f81e750bbee
snowthesprite <mpapaya9@gmail.com>
2020-08-24 22:22:36 +0000

    did nth term

--- recursive_sequence.py
+++ recursive_sequence.py
@@ -1,5 +1,8 @@
-def first_n_terms() :
-    
+def first_n_terms(n) :
+    if n == 1 :
+        return 5
+    return 3 * first_n_terms(n-1) - 4
 
-def nth_term() :
+#def nth_term() :
     
+print(first_n_terms(10))
