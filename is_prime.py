65d698bfe078c12fe7acfc6500d5dd3680d4cbf8
snowthesprite <mpapaya9@gmail.com>
2020-08-24 02:54:06 +0000

    the white rabbit made me do it

--- is_prime.py
+++ is_prime.py
@@ -2,8 +2,8 @@ def is_prime(num) :
     i = 2
     while i < num/2 :
         if num % i == 0 :
-        return False
-    i += 1
+            return False
+        i += 1
     return True
 
 num_1 = 59
