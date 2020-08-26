16885ecc37d71e4ba3269785db2b8b20a00f602c
snowthesprite <mpapaya9@gmail.com>
2020-08-26 21:42:15 +0000

    Finished Stack, everything works fine

--- stack.py
+++ stack.py
@@ -6,16 +6,23 @@ class Stack:
         self.data.append(new_data)
 
     def pop(self):
-        self.data.pop(len(data)-1)
+        self.data.pop(len(self.data)-1)
 
     def peek(self):
+        return self.data[-1]
         
 
 s = Stack()
+print(s.data)
 
 assert s.data == []
 
 s.push('a')
 s.push('b')
 s.push('c')
+print(s.data)
+s.pop()
+print(s.data)
+
+print(s.peek())
 print(s.data)
