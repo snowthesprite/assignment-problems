f8230b6db883d6a00acdfb2498398977ab56dff2
snowthesprite <mpapaya9@gmail.com>
2020-08-28 16:55:19 +0000

    Finished encode working on decode

--- linear_encoding_cryptography.py
+++ linear_encoding_cryptography.py
@@ -5,12 +5,23 @@ def encode(string, a, b) :
         translation.append(alphabet.index(char))
     i = 0
     while i < len(translation) :
-        translation[i] = a(translation[i]) + b
+        translation[i] = a * translation[i] + b
         i += 1
     return translation
 
 
-#def decode(numbers, a, b) :
+def decode(numbers, a, b) :
+    simplified = []
+    i = 0
+    while i < len(numbers) :
+        simplified.append((numbers[i] - b) / a)
+        i += 1
+    print(simplified)
+    part_trans = []
+    for num in simplified :
+        part_trans.append(alphabet[num])
+    translation = ''.join(emp_list)
+    return translation
 
 
-print(encode('a cat', 2, 3))
+print(decode([5, 3, 9, 5, 43], 2, 3))
