d0a5ab0c246a8af1c39883674fa825440e40028b
snowthesprite <mpapaya9@gmail.com>
2020-08-31 21:34:37 +0000

    Asserted Cryptography

--- linear_encoding_cryptography.py
+++ linear_encoding_cryptography.py
@@ -34,5 +34,20 @@ def fancy_decode(num_list) :
         
 fancy_translation = fancy_decode([377,717,71,513,105,921,581,547,547,105,377,717,241,71,105,547,71,377,547,717,751,683,785,513,241,547,751])
 
+print('Is {} the correct translation of {}'.format(decode([5, 3, 9, 5, 43], 2, 3), [5, 3, 9, 5, 43]))
+assert decode([5, 3, 9, 5, 43], 2, 3) == 'a cat', 'No, it is not the right translation'
+print('Yes, it is the correct translation')
+print('')
+
+print('Is {} the correct answer to {}'.format(decode([1, 3, 9, 5, 43], 2, 3),[1, 3, 9, 5, 43]))
+assert decode([1, 3, 9, 5, 43], 2, 3) == False, 'No, it is not the right answer'
+print('Yes it is the correct answer')
+print('')
+
+print('Is {} the correct answer to {}'.format(decode([5, 3, 9, 5, 44], 2, 3),[5, 3, 9, 5, 44]))
+assert decode([5, 3, 9, 5, 44], 2, 3) == False, 'No, it is not the right answer'
+print('Yes it is the correct answer')
+print('')
+
+
 print(fancy_translation[1])
-#print(fancy_decode([5, 3, 9, 5, 43]))
