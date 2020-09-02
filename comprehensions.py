7956fb0460779dd55fc4a4d40d3c5b621528c201
snowthesprite <mpapaya9@gmail.com>
2020-09-02 17:18:34 +0000

    Finished Dict section

--- comprehensions.py
+++ comprehensions.py
@@ -3,7 +3,7 @@ def even_odd_tuples(numbers) :
 
 def even_odd_dict(numbers) :
     #return {(n,'even') if n % 2 == 0 else (n,'odd') for n in numbers}
-    return {n : ('even' if n % 2 == 0 else 'odd)'  for n in numbers }
+    return {n : ('even' if n % 2 == 0 else 'odd')  for n in numbers }
 
 #print(even_odd_tuples([1,2,3,5,8,11]))
 
