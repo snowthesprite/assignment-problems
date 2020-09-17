d825df406dc6b477908d15ba665ca99b59795d1a
snowthesprite <mpapaya9@gmail.com>
2020-09-17 21:33:49 +0000

    Save

--- unlist.py
+++ unlist.py
@@ -1,21 +1,22 @@
-#def unlist_nonrecursive(x) :
+def unlist_nonrecursive(x) :
+    while False :
+        
     
 def unlist_recursive(x) :
+    if type(x) == int :
+        return x
     for blank in range(len(x)) :
         for blank_2 in range(len(x)) : 
-            if type(x[blank]) == int and type(x[-blank_2] == list :
-                print('run')
+            if type(x[blank]) == int and type(x[-blank_2]) == list :
                 return x
-    print(x)
     return unlist_recursive(x[0])
 
-print(unlist_recursive([[[[1], [2,3], 4]]]))
-print(unlist_recursive([[[[1]]]])) 
+print(unlist_nonrecursive([[[[1]]]]))
 
 #assert unlist_nonrecursive([[[[1], [2,3], 4]]]) == [[1], [2,3], 4]
 
 #assert unlist_nonrecursive([[[[1]]]]) == 1
 
-#assert unlist_recursive([[[[1], [2,3], 4]]]) == [([1], [2,3], 4)]
+assert unlist_recursive([[[[1], [2,3], 4]]]) == [[1], [2,3], 4]
 
-#assert unlist_recursive([[[[1]]]]) == 1
+assert unlist_recursive([[[[1]]]]) == 1
