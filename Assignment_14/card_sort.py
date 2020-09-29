05d2dab307102d8770bc3ef8ccc539e97a73bc40
snowthesprite <mpapaya9@gmail.com>
2020-09-29 19:33:23 +0000

    fix card_sort

--- Assignment_14/card_sort.py
+++ Assignment_14/card_sort.py
@@ -1,12 +1,14 @@
 def card_sort(num_list) :
     sorted_num_list = []
     for num in num_list :
-        #print('ran')
         i = 0
         run = False
         while i < len(sorted_num_list) :
-            if num > sorted_num_list[i] :
+            if num > sorted_num_list[i] and not run :
                 sorted_num_list.insert(i + 1, num)
+                run = True
+            elif num < sorted_num_list[i] and not run :
+                sorted_num_list.insert(i, num)
                 run = True
             i += 1
         if sorted_num_list == [] or not run :
