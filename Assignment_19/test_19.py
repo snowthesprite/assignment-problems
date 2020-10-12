abae251d30308e2507dff374368ba6d2e8a85144
snowthesprite <mpapaya9@gmail.com>
2020-10-12 21:10:55 +0000

    save

--- Assignment_19/test_19.py
+++ Assignment_19/test_19.py
@@ -2,10 +2,8 @@ from zero_of_tangent_line import *
 
 def f(x):
         return x**3 + x - 1
-        
+
 answer = zero_of_tangent_line(f, 0.5)
->>> round(answer,6)
-0.714286
->>> answer = estimate_solution(f, 0.5, 0.01)
->>> round(answer, 6)
-0.682328
+assert round(answer,6) == 0.714286
+answer = estimate_solution(f, 0.5, 0.01)
+assert round(answer, 6) == 0.682328
