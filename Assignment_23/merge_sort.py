2b5e8611a068cd6e17bfc3f14f8b50c86feb0792
snowthesprite <mpapaya9@gmail.com>
2020-10-25 01:52:12 +0000

    Took the wrong section of merge, time wasted

--- Assignment_23/merge_sort.py
+++ Assignment_23/merge_sort.py
@@ -4,28 +4,20 @@ def merge(x,y) :
     ran = False
     x_index = 0
     y_index = 0
-    print(len(x), len(y))
+    #print(x_index, y_index)
+    #print(len(x), len(y))
     merged_list = []
     while True :
-        if x_index >= len(x) and y_index > len(y) :
-            merged_list.append(y[y_index])
-            y_index += 1
-            ran = True
-        elif y_index >= len(y) and x_index < len(x) :
-            merged_list.append(x[x_index])
-            x_index += 1
-            ran = True
-        elif x_index >= len(x) and y_index >= len(y) :
+        if x_index >= len(x) and y_index >= len(y) :
             return merged_list
-        if not ran : 
-            x_element = x[x_index]
-            y_element = y[y_index]
-            if x_element <= y_element :
-                merged_list.append(x_element)
-                x_index += 1
-            elif y_element < x_element :
-                merged_list.append(y_element)
-                y_index += 1
+        x_element = x[x_index]
+        y_element = y[y_index]
+        if x_element <= y_element :
+            merged_list.append(x_element)
+            x_index += 1
+        elif y_element < x_element :
+            merged_list.append(y_element)
+            y_index += 1
 
 def merge_sort(given_list) :
     global a_repet
@@ -44,10 +36,11 @@ def merge_sort(given_list) :
     #'''
     if len(a) > 1 :
         a_list = [merge_sort(a)]
-    '''
-    elif len(a) == 1 :
+    print('ran')
+    #'''
+    if len(a) == 1 :
         sorted_list = merge(a, b)
-        print(sorted_list)
+        #print(sorted_list)
         
     '''
     if len(a) > 1 and len(b) > 1 :
--- main.sh
+++ main.sh
@@ -1,3 +1,5 @@
+#for when you can't commit again VVV
+#rm .git/index.lock
 python Assignment_23/test_23.py
 
 #python Quiz.py
