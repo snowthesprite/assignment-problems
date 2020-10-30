06119b7deec9309aba5256348c3e4894b9e7c185
snowthesprite <mpapaya9@gmail.com>
2020-10-30 01:45:08 +0000

    finished up with everything GODS I NEED TO ASSERT MORE I just end up working and working @ ~ @

--- Assignment_25/gradient_descent.py
+++ Assignment_25/gradient_descent.py
@@ -5,7 +5,7 @@ def estimate_y_derivative(f, point, delta) :
     return (f(point[0],( point[1] + 0.5 * delta)) - f(point[0], (point[1] - 0.5 * delta))) / delta
 
 def gradient_descent(f, inital_point, alpha=0.01, delta=0.0001, iterations=10000) :
-    current_point = [inital_point[0], inital_point[1]]
+    current_point = [inital_point[0], inital_point[1], f(inital_point[0], inital_point[1])]
     base_iterations = 0
     while base_iterations <= iterations :
         f_x = estimate_x_derivative(f, current_point, delta)
@@ -16,5 +16,6 @@ def gradient_descent(f, inital_point, alpha=0.01, delta=0.0001, iterations=10000
 
         current_point[1] = current_point[1] - alpha * f_y
 
+        current_point[2] = f(current_point[0], current_point[1])
         base_iterations += 1
-    return tuple(current_point)
+    return current_point[2]
--- Assignment_25/test_25.py
+++ Assignment_25/test_25.py
@@ -1,7 +1,27 @@
 from gradient_descent import *
 
-#a) Minimum is x = 1, y = 0
+#you didn't want the point last time so I didn't put the answer back into a tuple
 
 function = lambda x, y : 1 + x ** 2 + y ** 2
 
-print(gradient_descent(function, (1,2)))
+print('For 1 + x^2 + y^2 :')
+print('The predicted Minimum point is (0,0,1)')
+print('The calculated Minimum is {}'.format(gradient_descent(function, (1,2))))
+print()
+
+function = lambda x, y : 1 + x ** 2 + 2 * x + y ** 2 - 9 * y
+
+print('For 1 + x^2 + 2x + y^2 - 9y :')
+print('The predicted Minimum point is (-1, 4.5, -20.25)')
+
+'''
+Math behind it:
+f(x,y) = 1 + x^2 + 2x + y^2 - 9y
+    = x^2 + 2x + 1 + y^2 - 9y
+    = (x + 1)^2 + y^2 - 9y
+    = (x + 1)^2 + y^2 - 9y + (9/2)^2 - (9/2)^2
+    = (x + 1)^2 + y^2 - 9y + 20.25 - 20.25
+    = (x + 1)^2 + (y - 4.5)^2 - 20.25
+'''
+
+print('The calculated Minimum is {}'.format(gradient_descent(function, (0,0))))
