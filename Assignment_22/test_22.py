7a2594cf1baac5901f18b485c96cc0880fade7a7
snowthesprite <mpapaya9@gmail.com>
2020-11-12 23:51:26 +0000

    Added Assign 30

--- Assignment_22/test_22.py
+++ Assignment_22/test_22.py
@@ -1,6 +1,6 @@
 from gradient_descent import *
 import math
-
+'''
 def f(x):
     return x**2 + 2*x - 1
 
@@ -12,3 +12,9 @@ def g(x):
     return ((x**2) + math.cos(x))/ math.e ** math.sin(x)
 
 print('According to gradient descent, a minimum of g(x) will be {} '.format(gradient_descent(g, 0)))
+'''
+
+def f(x):
+    return 1 - x**2
+
+print(gradient_descent(f, 2))
--- main.sh
+++ main.sh
@@ -1,5 +1,5 @@
 #for when you can't commit again VVV
 #rm .git/index.lock
-python Assignment_25/test_25.py
+#python Assignment_22/test_22.py
 
-#python Quiz.py
+python Quiz.py
