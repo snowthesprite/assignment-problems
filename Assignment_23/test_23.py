381a83991ee6b07b4e76b32369a5fbc5f9adeafb
snowthesprite <mpapaya9@gmail.com>
2020-11-25 01:19:53 +0000

    Fixed up assignment 23

--- Assignment_23/test_23.py
+++ Assignment_23/test_23.py
@@ -1,3 +1,5 @@
 from merge_sort import *
 
-assert merge_sort([4,8,7,7,4,2,3,1]) == [1,2,3,4,4,7,7,8]
+print('Does merge_sort work?')
+assert merge_sort([4,8,7,7,4,2,3,1]) == [1,2,3,4,4,7,7,8], 'No, it does not'
+print('Yes it does')
