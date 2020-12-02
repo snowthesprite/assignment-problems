82b99eb75cbcb6e7762665342f8f308f78785a2f
snowthesprite <mpapaya9@gmail.com>
2020-12-02 04:59:37 +0000

    Finished certanty

--- Assignment_35/assignment_35_stats.py
+++ Assignment_35/assignment_35_stats.py
@@ -3,3 +3,10 @@ def guess_summation(func, start_term, max_term) :
     for term in range(start_term, max_term) :
         sum_guess += func(term)
     return sum_guess
+
+def certanty(percent, funct, bound) :
+    chance = 0
+    while chance < percent :
+        chance +=  funct(bound)
+        bound += 1
+    return bound
--- Assignment_35/test_35.py
+++ Assignment_35/test_35.py
@@ -5,3 +5,8 @@ def function(k) :
 
 guess = guess_summation(function, 25, 100000)
 print(1/guess)
+
+def function(k) :
+    return (1.4432 * 10 ** 6)/k**5
+
+print(certanty(0.95, function, 25))
