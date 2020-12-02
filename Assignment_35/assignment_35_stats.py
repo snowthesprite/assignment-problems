59bc53b90d2e7b2f53ee446815312b3ab0487ffc
snowthesprite <mpapaya9@gmail.com>
2020-12-02 02:28:53 +0000

    Working out bugs in Guess summation

---
+++ Assignment_35/assignment_35_stats.py
@@ -0,0 +1,6 @@
+def guess_summation(func, start_term, max_term) :
+    sum_guess = 0
+    for term in range(max_term, start_term) :
+        print(term)
+        sum_guess += func(term)
+    return sum_guess
---
+++ Assignment_35/test_35.py
@@ -0,0 +1,6 @@
+from assignment_35_stats import *
+
+def function(k) :
+    return 1/k**5
+
+print(guess_summation(function, 25, 1000))
