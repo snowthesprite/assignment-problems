c57ed81ce179338208dc3a8ba88c3a6210a16aa2
snowthesprite <mpapaya9@gmail.com>
2020-12-06 04:32:47 +0000

    Did asserts for Assign 37

--- Assignment_37/test_37.py
+++ Assignment_37/test_37.py
@@ -1,2 +1,31 @@
 from tree import *
 
+edges = [('a','c'), ('e','g'), ('e','i'), ('e','a'), ('d','b'), ('a','d'), ('d','f'), ('f','h'), ('d','j'), ('d','k')]
+
+print('Does the get_children function work?')
+
+assert get_children('e', edges) == ['g', 'i', 'a' ], 'Not for e'
+
+assert get_children('c', edges) == [], 'Not for c'
+
+assert get_children('f', edges) == ['h'], 'Not for f'
+
+print('Yes, it does!')
+print()
+
+print('Does the get_parents function work?')
+
+assert get_parents('e', edges) == [], 'Not for e'
+
+assert get_parents('c', edges) == ['a'], 'Not for c'
+
+assert get_parents('f', edges) == ['d'], 'Not for f'
+
+print('Yes, it does!')
+print()
+
+print('Does the get_root function work?')
+
+assert get_root(edges) == ['e'], 'No, it does not'
+
+print('Yes it does!')
