516b50ee10fc8da31c022ead3dee6bc7d7fbc700
snowthesprite <mpapaya9@gmail.com>
2021-01-11 03:09:26 +0000

    Fixed standard normal distribution Part 4 done

---
+++ Assignment_44/test_44.py
@@ -0,0 +1,7 @@
+from standard_normal_probability import *
+
+print('For -1 <= x <= 1 : ', calc_standard_normal_probability(-1, 1))
+print()
+print('For -2 <= x <= 2 : ',calc_standard_normal_probability(-2, 2))
+print()
+print('For -3 <= x <= 3 : ',calc_standard_normal_probability(-3, 3))
