e10a05df1f45f43a744e269b5725cef9ad751270
snowthesprite <mpapaya9@gmail.com>
2021-01-12 04:26:04 +0000

    All the graphs have funny spikes, find problem

--- Assignment_45/test_45.py
+++ Assignment_45/test_45.py
@@ -1,4 +1,5 @@
 from standard_normal_probability_2 import *
+import matplotlib.pyplot as plt
 ''''
 print(calc_standard_normal_probability(0, 1, 100, "left endpoint"))
 print()
@@ -8,6 +9,19 @@ print(calc_standard_normal_probability(0, 1, 100, "midpoint"))
 print()
 print(calc_standard_normal_probability(0, 1, 100, "trapezoidal"))
 print()
-'''
 print(calc_standard_normal_probability(0, 1, 20, "simpson"))
 print()
+'''
+keys = ["left endpoint", "right endpoint", "midpoint", "trapezoidal", "simpson"]
+
+plt.style.use('bmh')
+x_points = []
+y_points = {key : [] for key in keys}
+for subinterval_number in range(2, 101) :
+    x_points.append(subinterval_number)
+    for key in keys :
+        y_points[key].append(calc_standard_normal_probability(0, 1, subinterval_number, key))
+for key in keys :
+    plt.plot(x_points, y_points[key])
+plt.legend(keys)
+plt.savefig('Assignment_45/eulerplot.png')
