5f63f373f45f5e00ba5fc353a4e68423c73eecd8
snowthesprite <mpapaya9@gmail.com>
2021-01-13 01:01:07 +0000

    Made and finished complicated list Finished Assignment section

--- ComplicatedList.hs
+++ ComplicatedList.hs
@@ -1,1 +1,2 @@
-calcList n = len([
+calcList n = sum [ 1 | x <- [-n..n], y <- [-n..n], x >= -n, y <= n, x - y <= x * y / 2, x * y / 2 <= x + y, x /= -2, x /= -1, x /= 0, x /= 1, x /= 2, y /= -2, y /= -1, y /= 0, y /= 1, y /= 2]
+main = print (calcList 50)
