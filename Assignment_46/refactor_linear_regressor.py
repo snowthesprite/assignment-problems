4afed22195964d79dd24123e3a0aff3fbf66d418
snowthesprite <mpapaya9@gmail.com>
2021-01-15 00:02:06 +0000

    refactor_linear_regreser finished Assignment Done

--- Assignment_46/refactor_linear_regressor.py
+++ Assignment_46/refactor_linear_regressor.py
@@ -1,27 +1,22 @@
-    def calculate_coefficient(self):
-        finished_coeff = {}
-        print(list(self.df.data_dict.values()))
-        mat = [[1 for x in list(self.df.data_dict.values())[0]]]
-        independ_vars = {}
-        for key in self.df.data_dict:
-          if key != self.dependent_variable:
-            independ_vars[key] = self.df.data_dict[key]
-        for row in range(len(independ_vars)):
-          mat.append(list(self.df.data_dict.values())[row][0])
-        print(mat)
-        mat = Matrix(mat)
-        print(mat.elements)
-        mat = mat.transpose()
-        mat_t = mat.transpose()
-        mat_mult = mat_t.matrix_multiply(mat)
-        mat_inv = mat_mult.inverse()
-        mat_pseudoinv = mat_inv.matrix_multiply(mat_t)
-        multiplier = [[num] for num in list(self.df.data_dict.values())[1][0]]
-        multiplier_mat = mat_pseudoinv.matrix_multiply(Matrix(multiplier))
-        for num in range(len(multiplier_mat.elements)):
-          if num == 0:
-            key = 'constant'
-          else:
-            key = list(self.df.data_dict.keys())[num-1]
-          finished_coeff[key] = [row[0] for row in multiplier_mat.elements][num]
-        return finished_coeff
+def calculate_coefficient(self):
+    finished_coeff = {}
+    data_dict_data = list(self.df.data_dict.values())
+    line_eqn = [[1 for _ in data_dict_data[0]]]
+    independent_variable = {}
+    for key in self.df.data_dict:
+        if key != self.dependent_variable:
+            independent_variable[key] = self.df.data_dict[key]
+    for row in range(len(independent_variable)):
+        line_eqn.append(data_dict_data[row][0])
+    line_eqn = Matrix(line_eqn)
+    line_eqn_t = line_eqn.transpose()
+    line_eqn_mult = line_eqn_t.matrix_multiply(line_eqn)
+    line_eqn_inv = line_eqn_mult.inverse()
+    line_eqn_pseudoinv = line_eqn_inv.matrix_multiply(line_eqn_t)
+    multiplier = [data_dict_data[1][0]]
+    multiplier_x_pseudoinv = line_eqn_pseudoinv.matrix_multiply(Matrix(multiplier)).elements
+    finished_coeff['constant'] = multiplier_x_pseudoinv[0][0] #that should be the right idex
+    for num in range(len(multiplier_x_pseudoinv)):
+        key = list(self.df.data_dict)[num-1]
+        finished_coeff[key] = [row[0] for row in multiplier_x_pseudoinv][num]
+    return finished_coeff
