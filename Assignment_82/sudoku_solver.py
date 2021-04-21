8b38b69eda5080d977a99c14997c647e3600085e
snowthesprite <>
2021-04-21 05:47:19 +0000

    finished sudoku

--- Assignment_82/sudoku_solver.py
+++ Assignment_82/sudoku_solver.py
@@ -11,26 +11,22 @@ def solve_sudoku () :
                 box[num_id] = 1
             
             if is_valid(sudoku, box_id, num_id) :
-                num_id += 1
+                #print(box)
+                num_id = (num_id + 1) % 6
                 continue
 
             box[num_id] += 1
 
             if box[num_id] > len(box) :
                 new_box = reset(box_id)
-                #print(box, box[num_id])
                 while new_box[num_id] != None or box[num_id] > 5 :
                     box[num_id] = new_box[num_id]
                     num_id -= 1
-                    #print('num',box[num_id], num_id)
-                    #print('orig',new_box[num_id], num_id)
-                    print(box, new_box, '\n')
-                #print('final',box[num_id])
-                #t.sleep(1)
+                    #print(box, new_box, '\n')
                 box[num_id] += 1
         box_id += 1
-        print(box, '\n')
-    print(sudoku)
+        #print(box, '\n')
+    return sudoku
             
 
 def is_valid (puzzle, box_id, num_id) :
@@ -38,22 +34,24 @@ def is_valid (puzzle, box_id, num_id) :
     box = [puzzle[box_id][id] for id in range(6) if id != num_id]
     col = find_column(puzzle,box_id,num_id)
     row = find_row(puzzle, box_id, num_id)
-    print(num)
+    #print(num)
     if (num in box) or (num in col) or (num in row) :
-        print('Box:',num in box, puzzle[box_id])
-        print('Col:',num in col, col)
-        print('Row:',num in row, row, '\n')
-        t.sleep(3)
+        #print('Box:',num in box, puzzle[box_id])
+        #print('Col:',num in col, col)
+        #print('Row:',num in row, row, '\n')
+        #t.sleep(3)
         return False
-    print()
+    #print()
     return True
     
 def find_column (puzzle, box_id, num_id) :
+    start= box_id % 2
     alt_id = (num_id + 3) % 6
     column = []
-    for id in range(0,6,2) :
+    for id in range(start,6,2) :
         if id == box_id : 
             continue
+        #print(box_id,id)
         column.extend([puzzle[id][num_id], puzzle[id][alt_id]])
     return column
 
@@ -70,7 +68,7 @@ def find_row (puzzle, box_id, num_id) :
 
 def reset (id) :
     #start = [[None,None,4,None,None,None],[None,None,None,2,3,None],[3,None,None,None,6,None],[None,6,None,None,None,2],[None,2,1,None,None,None],[None,None,None,5,None,None]]
-    start = [[2,3,4,1,5,6],[None,None,None,2,3,None],[3,None,None,None,6,None],[None,6,None,None,None,2],[None,2,1,None,None,None],[None,None,None,5,None,None]]
+    start = [[2,3,4,1,5,6],[1,5,6,2,3,4],[3,1,2,4,6,5],[4,6,5,3,1,2],[5,2,1,6,4,3],[None,None,None,5,None,None]]
 
     if id == 'all' :
         return start
--- Assignment_82/test_82.py
+++ Assignment_82/test_82.py
@@ -1,3 +1,11 @@
 from sudoku_solver import *
 
-solve_sudoku()
+finished_sudoku = solve_sudoku()
+
+print('-----------------')
+for index in range(0,6,2) :
+    s_1 = finished_sudoku[index]
+    s_2 = finished_sudoku[index + 1]
+    for h in range(0,6,3) :
+        print('| {} {} {} | {} {} {} |'.format(s_1[h],s_1[h+1],s_1[h+2],s_2[h],s_2[h+1],s_2[h+2]))
+    print('-----------------')
